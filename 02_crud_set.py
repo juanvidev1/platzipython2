e37748396b725ed8ebd300a5f8b968a3872bb343
juanvidev1 <juanvidev1@gmail.com>
2023-01-19 11:40:35 -0500

    Added new file with crud operations over sets

---
+++ 02_crud_set.py
@@ -0,0 +1,17 @@
+"""
+EN - In this excersise we will learn about CRUD operations over data sets. Here we can see how to Create, Read, Update and Delete elements from a data set
+
+ES - En este ejercicio aprenderemos sobre las operaciones CRUD sobre los conjuntos. Aquí veremos cómo Crear, Obtener, Actualizar y Eliminar 
+elementos de un conjunto
+"""
+set_countries = {'Col', 'Mex', 'Bol'}
+
+"""
+EN - As other data structures in programming languages, you can do a lot of things with sets. The first one is show the length of the set 
+(the number of elements that the set contains). You can make this with the len() method of python
+
+ES - Como en otras estructuras de datos en los lenguajes de programación, puedes hacer muchas cosas con los conjuntos. 
+Una de las primeras es averiguar el tamaño del conjunto (el número de elementos que contiene). Puedes hacer esto con el método len() de python
+"""
+size = len(set_countries)
+print(size)
