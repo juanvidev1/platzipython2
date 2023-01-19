2e202c9827704bbed9ab18f694fd3e59f41fcd17
juanvidev1 <juanvidev1@gmail.com>
2023-01-19 15:29:11 -0500

    Finished first module: sets

--- 03_operations_set.py
+++ 03_operations_set.py
@@ -1,7 +1,7 @@
 """
-EN - In this class we will see how to do operations with sets.
+EN - In this class we will see how to do operations with sets. This operations only works with sets with common elements.
 
-ES - En esta clase veremos como hacer operaciones con los conjuntos
+ES - En esta clase veremos como hacer operaciones con los conjuntos. Estas operaciones sólo funcionan con elementos en común entre conjuntos 
 """
 set_a = {'Col', 'Mex', 'Bol'}
 set_b = {'Pe', 'Bol'}
@@ -40,4 +40,41 @@ set_c = set_a.intersection(set_b)
 print(set_c)
 
 set_c = set_a & set_b
+print(set_c)
+
+"""
+EN - Now we are go to talk about the difference operation. Basically consists in remove the common elements between a set_a and a set_b 
+and create a "new" set_c with the result of this sustraction.
+The sintax is: <set_a>.difference(<set_b>)
+The example below explains better this
+
+ES - Ahora vamos a hablar acerca de la operación de diferencia. Básicamente consiste en remover los elementos comunes entre un 
+conjunto_a y un conjunto_b para crear un "nuevo" conjunto_c con el resultado de esa resta.
+La sintaxis es: <conjunto_a>.difference(<conjunto_b>)
+El ejemplo de abajo explica mejor esto  
+"""
+set_c = set_a.difference(set_b)
+print(set_c)
+"""
+EN - Also has a math operator as other operations between sets
+
+ES - También tiene operadores matemáticos como las otras operaciones entre conjuntos
+"""
+set_c = set_a - set_b
+print(set_c)
+
+"""
+EN - Finally we're going to talk about Symmetric Difference. Here we remove all the common elements of both sets (a, b) 
+but those wich are not common to both sets (a, b).
+Sintax: <set_a>.symetric_difference(<set_b>)
+Operator syntax: <set_a> ^ <set_b> 
+
+ES - Finalmente hablaremos de la diferencia simétrica. Aquí se remueven todos los elementos comunes de los conjuntos (a, b) excepto aquellos elementos 
+que no sean comunes a ambos conjuntos (a, b).
+Sintaxis: <conjunto_a>.symetric_difference(<conjunto_b>)
+Sintaxis con operador: <conjunto_a> ^ <conjunto_b>
+"""
+set_c = set_a.symmetric_difference(set_b)
+print(set_c)
+set_c = set_a ^ set_b
 print(set_c)
